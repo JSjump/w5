417e5db5d3a9243de6164df0140428a607a92fcc
Yanbin Zhu <yanbinzhu@YanbindeMacBook-Pro.local>
2022-03-27 18:34:51 +0800

    feat(*): 培训第五周作业

---
+++ contracts/FirstToken.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FirstToken is ERC20 {
+    address public owner; 
+
+    constructor(uint256 _totalSupply) ERC20("FirstToken", "FT") {
+        // initialSupply = 0
+        _mint(msg.sender, _totalSupply);
+        owner = msg.sender;
+    }
+
+    // 可动态增发
+    function mint(uint256 num_) public onlyOwner {
+        _mint(msg.sender,num_);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender ==owner,"onlyOwner");
+        _;
+    }
+}
